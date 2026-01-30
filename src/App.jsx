85fd854d0bac1bf608d9d8f8974ab4bbab968eae
Lavanya <znac542@live.rhul.ac.uk>
2026-01-30 12:54:31 +0000

    landing page + reactbits background

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
+import Silk from "./Silk";
 
 function App() {
     return (
-        <>
-            {/* Navbar */}
+        <div className="app-container">
+            <div className="background-canvas">
+                <Silk color="#b14f6f" speed={6.7} scale={1.3} noiseIntensity={1.5} />
+            </div>
+
             <nav className="navbar">
                 <div className="nav-left">Lush Looks</div>
-
                 <div className="nav-right">
                     <a href="#about">About</a>
                     <a href="#services">Services</a>
@@ -14,13 +17,12 @@ function App() {
                 </div>
             </nav>
 
-            {/* Hero Section */}
             <main className="hero">
-                <h1>Relaxed Beauty,<br /> Lush Looks</h1>
+                <h1>Relaxed Beauty, Lush Looks</h1>
                 <p>A mobile beauty and hair service</p>
                 <button className="cta-btn">Book Now</button>
             </main>
-        </>
+        </div>
     );
 }
 
